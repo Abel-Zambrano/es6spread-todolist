63b3abae68fbdc6f044d0ae40c5365b88e6cb7e1
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-03 12:58:37 -0700

    updated app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([])
+
+  function changeList(event) {
+    const newValue = event.target.value;
+
+    setTodo(newValue);
+  }
+
+  function addItem() {
+    setList((firstValue) => {
+      return [...firstValue, todo];
+    });
+    setTodo("");
+  }
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
+        <form action="">
+          <input onChange={changeList} type="text" value={todo} required/>
+          <button onClick={addItem}>
+            <span>Add</span>
+          </button>
+        </form>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map(item => 
+          <li>{item}</li>
+          )}
         </ul>
       </div>
     </div>
